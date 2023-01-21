7e7dcd779754fddb8f238ad9b31f6319c57ee756
munshihasan <122831227+munshihasan@users.noreply.github.com>
2023-01-21 10:24:42 -0500

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,9 @@
+package com.qa.opencart.constants;
+
+public class Constants {
+
+	public static final String LOGIN_PAGE_TITLE = "account login";
+	
+	
+	
+}
---
+++ DriverFactory.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.factory;
+
+public class DriverFactory {
+	
+	WebDriver driver;
+	
+	public void init-driver(String browserName){
+		
+		System.out.println("browser name is:" + browserName);
+	}
+
+}
